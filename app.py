f0e6500d59ad34a54af7ea925265d551e87f33ea
NAFISA NOURIN <nafisanourin2004@gmail.com>
2026-04-03 16:53:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+
+# Load model
+model = pickle.load(open('model.pkl', 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+
+# Preprocessing
+def preprocess(text):
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    words = text.split()
+    words = [w for w in words if w not in stopwords.words('english')]
+    return " ".join(words)
+
+# UI
+st.title("📰 Fake News Detection System")
+
+input_text = st.text_area("Enter News Text")
+
+if st.button("Predict"):
+    processed = preprocess(input_text)
+    vector = vectorizer.transform([processed])
+    result = model.predict(vector)
+    
+    st.subheader("Result:")
+    st.success(result[0])
